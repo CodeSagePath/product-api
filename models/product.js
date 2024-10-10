9f0bed3fc0f541d7e1b83c11dd1e195ed64b167c
CodeSagePath <rishigarg185@gmail.com>
2024-10-10 18:40:54 +0530

    added product model and data types, accroding to given specification

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'products',
+  timestamps: false,
+});
+
+module.exports = Product;
